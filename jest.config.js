154ee90dc09b65fd2ab80c1fd4f11fe5f75d33d2
Ikechukwu Kalu <ikechukwuakalu@gmail.com>
2019-10-18 19:57:50 +0100

    Modify config files

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,9 @@ module.exports = {
   clearMocks: true,
   coverageDirectory: "coverage",
   testEnvironment: "node",
+  transform: {
+    ".(ts|tsx)": "ts-jest"
+  },
+  testRegex: "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
+  moduleFileExtensions: ["ts", "tsx", "js"]
 };
